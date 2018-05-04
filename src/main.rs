4dddb6608f4b61cc696499b31db924121199d265
Attila Szeremi <attila@szeremi.org>
2018-05-04 15:57:43 +0200

    Change algorithm again, once again not gaining performance

--- src/main.rs
+++ src/main.rs
@@ -5,34 +5,33 @@ fn main() {
 fn crack(password :&str, starting: &str) -> bool {
     let mut current_string = starting.to_string();
 
-    let mut a = 'z' as u8 + 1;
     loop {
-        if a > 'z' as u8 {
-            let mut depth = 0;
-            while let Some(last_char) = current_string.pop() {
-                if last_char == 'z' {
-                    depth += 1;
-                    continue;
-                } else {
-                    current_string.push(((last_char as u8) + 1) as char);
-                    depth -= 1;
-                    break;
-                }
+        let mut a = 'a' as u8;
+        while a <= ('z' as u8) {
+            current_string.push(a as char);
+            if password == &current_string {
+                println!("Found: {}", current_string);
+                return true;
             }
-            while depth > 0 {
-                current_string.push('a');
-                depth -= 1;
-            }
-            a = 'a' as u8;
-        } else {
+//            println!("Current: {}", current_string);
             current_string.pop();
+            a += 1;
         }
-        current_string.push(a as char);
-        if password == &current_string {
-            println!("Found: {}", current_string);
-            return true;
+
+        let mut depth = 1;
+        while let Some(last_char) = current_string.pop() {
+            if last_char == 'z' {
+                depth += 1;
+                continue;
+            } else {
+                current_string.push(((last_char as u8) + 1) as char);
+                depth -= 1;
+                break;
+            }
         }
-//        println!("Current: {}", current_string);
-        a += 1;
+        while depth > 0 {
+            current_string.push('a');
+            depth -= 1;
+        }
     }
 }
