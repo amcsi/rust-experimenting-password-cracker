8131b576247c362d4afe3b0e5173a8c5442d2ff3
Attila Szeremi <attila@szeremi.org>
2018-05-08 16:31:51 +0200

    Use password iterator; time is 27sec; used to be 5sec :(

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use rayon::prelude::*;
 use std::str;
 
 fn main() {
-    crack("zzzzz", &Vec::new());
+    crack("zzzzz");
 }
 
 struct PasswordToCrack<'a> {
@@ -33,43 +33,39 @@ fn generate_string(mut i: u64) -> Vec<u8> {
     result.into_iter().rev().collect()
 }
 
+struct PasswordIterator {
+    i: u64,
+}
 
-fn crack(password :&str, starting: &Vec<u8>) -> bool {
+impl PasswordIterator {
+    fn new() -> Self {
+        PasswordIterator { i: 0 }
+    }
+}
+
+impl Iterator for PasswordIterator {
+    type Item = Vec<u8>;
+
+    fn next(&mut self) -> Option<Vec<u8>> {
+        let i = self.i;
+        self.i += 1;
+        return Some(generate_string(i));
+    }
+}
+
+fn crack(password: &str) -> bool {
     let password_bytes = Vec::from(password);
-    let mut current_string:Vec<u8> = starting.clone();
 
-    loop {
-        let mut a = 'a' as u8;
-        while a <= ('z' as u8) {
-            current_string.push(a);
-            if &password_bytes == &current_string {
-                match String::from_utf8(current_string) {
-                    Ok(found_string) => println!("Found: {}", found_string),
-                    Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
-                }
-                return true;
+    for current_string in PasswordIterator::new() {
+        if &password_bytes == &current_string {
+            match String::from_utf8(current_string) {
+                Ok(found_string) => println!("Found: {}", found_string),
+                Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
             }
-//            println!("Current: {}", current_string);
-            current_string.pop();
-            a += 1;
-        }
-
-        let mut depth = 1;
-        while let Some(last_char) = current_string.pop() {
-            if last_char == 'z' as u8  {
-                depth += 1;
-                continue;
-            } else {
-                current_string.push((last_char as u8) + 1);
-                depth -= 1;
-                break;
-            }
-        }
-        while depth > 0 {
-            current_string.push('a' as u8);
-            depth -= 1;
+            return true;
         }
     }
+    return false;
 }
 
 #[test]
